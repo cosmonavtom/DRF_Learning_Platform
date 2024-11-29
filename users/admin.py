8e4858be2b1c5cb4936b593dbfc04faac91d5af2
cosmonavtom <bedbmakc@gmail.com>
2024-11-29 15:54:04 +0300

    Написана админка

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ Модель User зарегистрирована в админке"""
+    list_display = ('id','email','first_name','last_name', 'is_active',)
+    list_filter = ('id','email',)
+    ordering = ('id',)
