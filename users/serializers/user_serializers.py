8d5999568a3e5cf57ba5d15e19de9fdf481a2840
cosmonavtom <bedbmakc@gmail.com>
2024-11-29 15:54:28 +0300

    Написан user_serializers.py

---
+++ users/serializers/user_serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainSerializer, TokenObtainPairSerializer
+
+from users.validators import PasswordValidator
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'is_active']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8, max_length=16)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password']
+        validators = [PasswordValidator(field='password')]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.set_password(user.password)
+        user.save()
+        return user
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password', 'phone', 'is_active']
+        validators = [
+            PasswordValidator(field='password')
+        ]
+
+
+class UserToTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email']=user.email
+        return token
